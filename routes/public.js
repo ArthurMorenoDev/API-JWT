305e41f01da9d6718b46bfc86074a7adc36711ed
Arthur <arthurmoreno31@gmail.com>
2024-09-18 15:58:00 -0300

    *

--- routes/public.js
+++ routes/public.js
@@ -10,28 +10,47 @@ const router = express.Router()
 const JWT_SECRET = process.env.JWT_SECRET
 
 // Cadastro
+
 router.post('/cadastro', async (req, res) => {
   try {
-    const user = req.body
+    const { name, email, departament, password } = req.body;
 
-    const salt = await bcrypt.genSalt(10)
-    const hashPassword = await bcrypt.hash(user.password, salt)
+    // Verifica se todos os campos obrigatórios estão presentes
+    if (!name || !email || !departament || !password) {
+      return res.status(400).json({ message: 'Preencha todos os campos obrigatórios.' });
+    }
 
+    // Verifica se o email já existe
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email já cadastrado.' });
+    }
+
+    // Cria o hash da senha
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // Salva o usuário no banco de dados
     const userDB = await prisma.user.create({
       data: {
-        name: user.name,
-        email: user.email,
-        departament: user.departament,
+        name,
+        email,
+        departament,
         password: hashPassword,
       },
-    })
-    res.status(201).json(userDB)
-  } catch (err) {
-    res.status(500).json({ message: 'Erro no Servidor, tente novamente' })
-  }
-})
+    });
 
-//lOGIN
+    res.status(201).json(userDB);
+  } catch (err) {
+    console.error('Erro ao cadastrar usuário:', err); // Log do erro no servidor
+    res.status(500).json({ message: 'Erro no servidor, tente novamente mais tarde.' });
+  }
+});
+
+
+// Login
 router.post('/login', async (req, res) => {
   try {
     const userInfo = req.body
@@ -54,9 +73,21 @@ router.post('/login', async (req, res) => {
     }
 
     // Gerar o Token JWT
-    const token = jwt.sign({ id: user.id}, JWT_SECRET, { expiresIn: '1d'})
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1d' })
 
-    res.status(200).json({token,userId:user.id  })
+    // Buscar o usuário pelo ID novamente para obter todos os detalhes
+    const userById = await prisma.user.findUnique({
+      where: { id: user.id },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        departament: true,
+        tabulacoes: true,
+      },
+    })
+
+    res.status(200).json({ token, user: userById })
   } catch (err) {
     res.status(500).json({ message: 'Erro no Servidor, tente novamente' })
   }
